66e436abbfd7c2572f5b893fd7ae843adbc3d667
Chris Wiederspan <chwieder@microsoft.com>
2019-03-29 07:09:54 -0600

    Updated API service to include health check result

---
+++ Backend/Terrademo.Backend/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Terrademo.Backend.Services;
+using Terrademo.Backend.Models;
+using System.Net;
+
+namespace Terrademo.Backend.Controllers {
+    
+    [ApiController]
+    public class HealthController : ControllerBase {
+
+        // GET api/resources
+        [HttpGet("")]
+        public IActionResult Get() {
+
+            return this.Ok("Service is healthy");
+        }
+    }
+}
