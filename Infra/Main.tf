22d8328ba819e7f31a12352210a42f8a26790f65
Chris Wiederspan <chwieder@microsoft.com>
2019-08-29 07:05:48 -0600

    Updated the terraform to v12

--- Infra/Main.tf
+++ Infra/Main.tf
@@ -1,63 +1,68 @@
-provider "azurerm" {
-  version = "=1.23.0"
-}
-
 terraform {
+  required_version = ">= 0.12"
+
   backend "azurerm" {
     environment = "public"
   }
 }
 
-variable "resource_group_name" { }
+provider "azurerm" {
+  version = "=1.33.1"
+}
 
-variable "container_registry_name" { }
+variable "resource_group_name" {
+}
 
-variable "app_insights_name" { }
+variable "container_registry_name" {
+}
 
-variable "app_service_plan_name" { }
+variable "app_insights_name" {
+}
 
-variable "frontend_app_name" { }
+variable "app_service_plan_name" {
+}
 
-variable "backend_app_name" { }
+variable "frontend_app_name" {
+}
 
-variable "location" { }
+variable "backend_app_name" {
+}
 
+variable "location" {
+}
 
 # *** Start Resource Group *** #
 
 resource "azurerm_resource_group" "group" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
+  name     = var.resource_group_name
+  location = var.location
 }
-
 
 # *** Start Azure Container Registry (ACR) *** #
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.container_registry_name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
   admin_enabled       = true
   sku                 = "Basic"
 }
 
-
 # *** Start Application Insights *** #
 
 resource "azurerm_application_insights" "insights" {
-  name                = "${var.app_insights_name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
+  name                = var.app_insights_name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
   application_type    = "Web"
 }
-
 
 # *** Start App Service Plan *** #
 
 resource "azurerm_app_service_plan" "plan" {
-  name                = "${var.app_service_plan_name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
+  name                = var.app_service_plan_name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
   kind                = "Linux"
   reserved            = true
 
@@ -67,68 +72,68 @@ resource "azurerm_app_service_plan" "plan" {
   }
 }
 
-
 # *** Start App Service for Site Backend API *** #
 
 resource "azurerm_app_service" "backend" {
-  name                = "${var.backend_app_name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
-  app_service_plan_id = "${azurerm_app_service_plan.plan.id}"
+  name                = var.backend_app_name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
   # kind                = "app,linux,container"
-  
+
   site_config {
-    always_on         = true
-    linux_fx_version  = "DOCKER|${azurerm_container_registry.acr.name}.azurecr.io/backend:latest"
+    always_on        = true
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.name}.azurecr.io/backend:latest"
     # default_documents = [ "Index.html" ]
   }
-  
-  app_settings {
+
+  app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
     DOCKER_REGISTRY_SERVER_URL          = "https://${azurerm_container_registry.acr.name}.azurecr.io"
     DOCKER_CUSTOM_IMAGE_NAME            = "https://${azurerm_container_registry.acr.name}.azurecr.io/backend:latest"
-    DOCKER_REGISTRY_SERVER_USERNAME     = "${azurerm_container_registry.acr.name}"
-    DOCKER_REGISTRY_SERVER_PASSWORD     = "${azurerm_container_registry.acr.admin_password}"
+    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.acr.name
+    DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.acr.admin_password
   }
 
   lifecycle {
     ignore_changes = [
-      "app_settings.DOCKER_CUSTOM_IMAGE_NAME",
-      "site_config.0.linux_fx_version",
-      "site_config.0.scm_type"
+      app_settings.DOCKER_CUSTOM_IMAGE_NAME,
+      site_config.0.linux_fx_version,
+      site_config.0.scm_type
     ]
   }
 }
 
-
 # *** Start App Service for Site Frontend Client *** #
 
 resource "azurerm_app_service" "frontend" {
-  name                = "${var.frontend_app_name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
-  app_service_plan_id = "${azurerm_app_service_plan.plan.id}"
+  name                = var.frontend_app_name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
   # kind                = "app,linux,container"
-  
+
   site_config {
-    always_on         = true
-    linux_fx_version  = "DOCKER|${azurerm_container_registry.acr.name}.azurecr.io/frontend:latest"
+    always_on        = true
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.name}.azurecr.io/frontend:latest"
     # default_documents = [ "Index.html" ]
   }
-  
-  app_settings {
+
+  app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
     DOCKER_REGISTRY_SERVER_URL          = "https://${azurerm_container_registry.acr.name}.azurecr.io"
     DOCKER_CUSTOM_IMAGE_NAME            = "https://${azurerm_container_registry.acr.name}.azurecr.io/frontend:latest"
-    DOCKER_REGISTRY_SERVER_USERNAME     = "${azurerm_container_registry.acr.name}"
-    DOCKER_REGISTRY_SERVER_PASSWORD     = "${azurerm_container_registry.acr.admin_password}"
+    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.acr.name
+    DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.acr.admin_password
   }
 
   lifecycle {
     ignore_changes = [
-      "app_settings.DOCKER_CUSTOM_IMAGE_NAME",
-      "site_config.0.linux_fx_version",
-      "site_config.0.scm_type"
+      app_settings.DOCKER_CUSTOM_IMAGE_NAME,
+      site_config.0.linux_fx_version,
+      site_config.0.scm_type
     ]
   }
 }
--- Infra/gateway.tf
+++ Infra/gateway.tf
@@ -1,28 +1,32 @@
-variable "gateway_name" { }
+variable "gateway_name" {
+}
 
-variable "storage_name" { }
+variable "storage_name" {
+}
 
 variable "gateway_instance_count" {
   default = 1
 }
 
-variable "public_ip_name" { }
+variable "public_ip_name" {
+}
 
-variable "vnet_name" { }
+variable "vnet_name" {
+}
 
-variable "ssl_filename" { }
+variable "ssl_filename" {
+}
 
-variable "ssl_password" { }
-
+variable "ssl_password" {
+}
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.storage_name}"
-  resource_group_name      = "${azurerm_resource_group.group.name}"
-  location                 = "${azurerm_resource_group.group.location}"
+  name                     = var.storage_name
+  resource_group_name      = azurerm_resource_group.group.name
+  location                 = azurerm_resource_group.group.location
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
   # Update: This is now taking place in the Azure DevOps pipeline
   # This should use native functionality to turn on static sites for the azurerm, but it's not available yet.
   # Read More: https://github.com/terraform-providers/terraform-provider-azurerm/issues/1903
@@ -32,31 +36,30 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.vnet_name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
+  name                = var.vnet_name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
   address_space       = ["10.10.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "gateway-subnet"
-  resource_group_name  = "${azurerm_resource_group.group.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name  = azurerm_resource_group.group.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "10.10.1.0/24"
 }
 
 resource "azurerm_public_ip" "ip" {
-  name                = "${var.public_ip_name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
-  domain_name_label   = "${var.gateway_name}"
+  name                = var.public_ip_name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
+  domain_name_label   = var.gateway_name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 # since these variables are re-used - a locals block makes this more maintainable
 locals {
-
   backend_address_pool_name      = "${var.gateway_name}-bepool"
   frontend_port_name             = "${var.gateway_name}-feport"
   frontend_ip_configuration_name = "${var.gateway_name}-feip"
@@ -71,19 +74,19 @@ locals {
 }
 
 resource "azurerm_application_gateway" "gateway" {
-  name                = "${var.gateway_name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
+  name                = var.gateway_name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
 
   sku {
     name     = "Standard_v2"
     tier     = "Standard_v2"
-    capacity = "${var.gateway_instance_count}"
+    capacity = var.gateway_instance_count
   }
 
   gateway_ip_configuration {
-    name      = "${local.gateway_ip_config_name}"
-    subnet_id = "${azurerm_subnet.subnet.id}"
+    name      = local.gateway_ip_config_name
+    subnet_id = azurerm_subnet.subnet.id
   }
 
   frontend_port {
@@ -97,102 +100,103 @@ resource "azurerm_application_gateway" "gateway" {
   }
 
   frontend_ip_configuration {
-    name                 = "${local.frontend_ip_configuration_name}"
-    public_ip_address_id = "${azurerm_public_ip.ip.id}"
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.ip.id
   }
 
   backend_address_pool {
     name  = "${local.backend_address_pool_name}-www"
-    fqdns = ["${azurerm_app_service.frontend.default_site_hostname}"]
+    fqdns = [azurerm_app_service.frontend.default_site_hostname]
   }
 
   backend_address_pool {
     name  = "${local.backend_address_pool_name}-api"
-    fqdns = ["${azurerm_app_service.backend.default_site_hostname}"]
+    fqdns = [azurerm_app_service.backend.default_site_hostname]
   }
 
   # TODO: This should use an export value from the storage account once that functionality is available
   backend_address_pool {
     name  = "${local.backend_address_pool_name}-util"
-    fqdns = ["${var.storage_name}.z5.web.core.windows.net"]   # Hard-coded for now, until azurerm can handle setting up static site in storage
+    fqdns = ["${var.storage_name}.z5.web.core.windows.net"] # Hard-coded for now, until azurerm can handle setting up static site in storage
   }
 
   backend_http_settings {
-    name                  = "${local.http_setting_name}"
-    cookie_based_affinity = "Disabled"
-    port                  = 443
-    protocol              = "https"
-    request_timeout       = 1
-    probe_name            = "${local.probe_name}"
+    name                                = local.http_setting_name
+    cookie_based_affinity               = "Disabled"
+    port                                = 443
+    protocol                            = "https"
+    request_timeout                     = 1
+    probe_name                          = local.probe_name
     pick_host_name_from_backend_address = "true"
   }
 
   http_listener {
     name                           = "${local.listener_name}-http"
-    frontend_ip_configuration_name = "${local.frontend_ip_configuration_name}"
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = "${local.frontend_port_name}-http"
     protocol                       = "http"
   }
 
   http_listener {
     name                           = "${local.listener_name}-https"
-    frontend_ip_configuration_name = "${local.frontend_ip_configuration_name}"
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = "${local.frontend_port_name}-https"
     protocol                       = "https"
-    ssl_certificate_name           = "${local.ssl_name}"
+    ssl_certificate_name           = local.ssl_name
   }
 
   ssl_certificate {
-    name       = "${local.ssl_name}"
-    data       = "${file("${var.ssl_filename}")}"
-    password   = "${var.ssl_password}"
+    name     = local.ssl_name
+    data     = file(var.ssl_filename)
+    password = var.ssl_password
   }
 
   probe {
-    name                = "${local.probe_name}"
-    protocol            = "https"
-    path                = "/"
-    interval            = 30
-    timeout             = 30
-    unhealthy_threshold = 3
+    name                                      = local.probe_name
+    protocol                                  = "https"
+    path                                      = "/"
+    interval                                  = 30
+    timeout                                   = 30
+    unhealthy_threshold                       = 3
     pick_host_name_from_backend_http_settings = "true"
   }
 
   request_routing_rule {
-    name                           = "${local.request_routing_rule_name}-http"
-    rule_type                      = "PathBasedRouting"
-    http_listener_name             = "${local.listener_name}-http"
-    url_path_map_name              = "${local.url_path_map_name}"
+    name               = "${local.request_routing_rule_name}-http"
+    rule_type          = "PathBasedRouting"
+    http_listener_name = "${local.listener_name}-http"
+    url_path_map_name  = local.url_path_map_name
   }
 
   request_routing_rule {
-    name                           = "${local.request_routing_rule_name}-https"
-    rule_type                      = "PathBasedRouting"
-    http_listener_name             = "${local.listener_name}-https"
-    url_path_map_name              = "${local.url_path_map_name}"
+    name               = "${local.request_routing_rule_name}-https"
+    rule_type          = "PathBasedRouting"
+    http_listener_name = "${local.listener_name}-https"
+    url_path_map_name  = local.url_path_map_name
   }
 
   url_path_map {
-    name                               = "${local.url_path_map_name}"
+    name                               = local.url_path_map_name
     default_backend_address_pool_name  = "${local.backend_address_pool_name}-www"
-    default_backend_http_settings_name = "${local.http_setting_name}"
-    
+    default_backend_http_settings_name = local.http_setting_name
+
     path_rule {
       name                       = "${local.url_path_map_rule_name}-api"
       backend_address_pool_name  = "${local.backend_address_pool_name}-api"
-      backend_http_settings_name = "${local.http_setting_name}"
+      backend_http_settings_name = local.http_setting_name
       paths = [
-        "/api/*"
+        "/api/*",
       ]
     }
-    
+
     path_rule {
       name                       = "${local.url_path_map_rule_name}-util"
       backend_address_pool_name  = "${local.backend_address_pool_name}-util"
-      backend_http_settings_name = "${local.http_setting_name}"
+      backend_http_settings_name = local.http_setting_name
       paths = [
-        "/.well-known/acme-challenge/*"
+        "/.well-known/acme-challenge/*",
       ]
     }
   }
 }
+
